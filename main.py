b3754d93920c209dc75110eb9cbfaab9b1a8e1f2
潘朝思 <panchaosi@bytedance.com>
2022-02-25 22:18:47 +0800

    feat: use loguru

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python3
 import os
 import asyncio
+from loguru import logger as log
 from telethon.sync import TelegramClient, errors
 from telethon.tl.types import InputMessagesFilterPhotos
 from telethon.tl.types import InputMessagesFilterVideo
@@ -15,7 +16,7 @@ max_worker_num = 8
 dir_prefix = './downloads/'
 delete_exist = True
 proxy = ("socks5", '127.0.0.1', 10808)
-client = TelegramClient(session_name, api_id, api_hash, proxy=proxy)
+client = TelegramClient(session_name, api_id, api_hash, proxy=None)
 
 
 async def get_chat_id_by_name(_client, _name):
@@ -31,7 +32,7 @@ async def fetch_message(q, v_chat, v_filter):
             filename = f'{message.id}{message.file.ext}'
         message.file_type = v_filter.__name__[19:]
         message.file_name = f'[({message.file_type})({message.id}){message.message}]{filename}'
-        print('fetch message', message.file_name)
+        log.info('fetch message {}', message.file_name)
         await q.put(message)
 
 
@@ -40,7 +41,7 @@ async def download_worker(q, i):
         message = await q.get()
         dirname = dir_prefix + f'({message.chat.title}{message.chat.id})/' + message.date.strftime("%Y-%m")
         filepath = os.path.join(dirname, message.file_name)
-        # print('worker', i, 'start to download', message.file_name)
+        # log.info('worker', i, 'start to download', message.file_name)
         try:
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
@@ -48,23 +49,23 @@ async def download_worker(q, i):
                 if os.path.getsize(filepath) < message.file.size:
                     os.remove(filepath)
                 else:
-                    print('already exist file', message.file_name)
+                    log.info('already exist file {}', message.file_name)
                     continue
-            # await asyncio.sleep(1)
-            await client.download_media(message, filepath)
-            print('successfully download', message.file_name)
+            await asyncio.sleep(1)
+            # await client.download_media(message, filepath)
+            log.info('successfully download {}', message.file_name)
         except errors.rpc_errors_re.FileReferenceExpiredError:
             async for msg in client.iter_messages(message.chat, offset_id=message.id - 1, reverse=True, limit=1):
                 await q.put(msg)
         except Exception as e:
-            print(f'{e}')
+            log.error(f'{e}')
         finally:
             q.task_done()
 
 
 async def main():
     q = asyncio.Queue()
-    name, chat = await get_chat_id_by_name(client, '全球新鲜事')
+    name, chat = await get_chat_id_by_name(client, '隔壁')
 
     _types = [InputMessagesFilterVideo, InputMessagesFilterPhotos, InputMessagesFilterDocument,
               InputMessagesFilterMusic, InputMessagesFilterVoice]
@@ -85,12 +86,13 @@ async def main():
 
 
 if __name__ == '__main__':
+    log.add("./logs/{time:YYYY-MM-DD__HH}.log", rotation="1h")
     client.start()
     loop = asyncio.get_event_loop()
     try:
-        print('----start!----')
+        log.info('----start to download!----')
         loop.run_until_complete(main())
     finally:
         client.disconnect()
         loop.close()
-        print('----download finished, gracefully stopped!----')
+        log.info('----download finished, gracefully stopped!----')
